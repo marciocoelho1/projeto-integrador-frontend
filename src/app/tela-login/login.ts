bdd01abf7088ff164f241d973ee8739fb1aae4f0
Marcio Coelho <marcio.coelho.jr1@gmail.com>
2026-07-30 20:32:10 -0300

    feat: estrutura inicial do projeto frontend angular

---
+++ src/app/tela-login/login.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../service/auth';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  email = '';
+  senha = '';
+  
+  constructor(private authService: AuthService){}
+
+  entrar(){
+    this.authService.login({
+      email: this.email,
+      senha: this.senha
+    }).subscribe({
+      next: (res) => {
+        console.log('Resposta da API:', res);
+        this.authService.salvarToken(res.access_token);
+      },
+      error: () => {
+        console.log('Erro no login');
+      }
+    });
+  }
+}
